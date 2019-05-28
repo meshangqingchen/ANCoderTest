6570fdac40f506bcc241fd0c86accc08cf86dfab
lichangcheng <13072655185@163.com>
2019-05-28 20:37:34 +0800

    图片浏览器

--- app/src/main/java/com/example/ancodertest/MainClassTypeListModel.java
+++ app/src/main/java/com/example/ancodertest/MainClassTypeListModel.java
@@ -4,6 +4,7 @@ import android.widget.TextView;
 
 import com.example.ancodertest.FrameLayout.FrameLayoutActivity;
 import com.example.ancodertest.GridLayout.GridLayoutActivity;
+import com.example.ancodertest.ImageView.ImageViewActivity;
 import com.example.ancodertest.LinearLayout.LinearLayoutActivity;
 import com.example.ancodertest.RelativeLayout.RelativeLayoutActivity;
 import com.example.ancodertest.TableLayout.TableLayoutActivity;
@@ -18,7 +19,8 @@ public class MainClassTypeListModel {
             new MainClassTypeModel("TableLayoutActivity","测试丰富的表格布局", TableLayoutActivity.class),
             new MainClassTypeModel("FrameLayoutActivity","测试帧布局", FrameLayoutActivity.class),
             new MainClassTypeModel("RelativeLayoutActivity","相对布局", RelativeLayoutActivity.class),
-            new MainClassTypeModel("GridLayoutActivity","网格布局", GridLayoutActivity.class)
+            new MainClassTypeModel("GridLayoutActivity","网格布局", GridLayoutActivity.class),
+            new MainClassTypeModel("ImageViewActivity","imageView图片浏览器", ImageViewActivity.class)
     );
 
     public List<MainClassTypeModel> getDataList() {

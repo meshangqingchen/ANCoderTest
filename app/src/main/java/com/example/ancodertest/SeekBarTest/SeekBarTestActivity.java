fb8d300705fbf548eba1d8281b1e8cea4ac4eaad
lichangcheng <13072655185@163.com>
2019-05-30 15:00:38 +0800

    ==

--- app/src/main/java/com/example/ancodertest/SeekBarTest/SeekBarTestActivity.java
+++ app/src/main/java/com/example/ancodertest/SeekBarTest/SeekBarTestActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
 
 import com.example.ancodertest.R;
@@ -21,6 +22,10 @@ public class SeekBarTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_seek_bar_test);
         image = (ImageView) findViewById(R.id.image);
+
+        float alpha = image.getImageAlpha();
+
+
         SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
         {
@@ -35,12 +40,29 @@ public class SeekBarTestActivity extends AppCompatActivity {
             @Override
             public void onStartTrackingTouch(SeekBar bar)
             {
+                System.out.println("star");
             }
             @Override
             public void onStopTrackingTouch(SeekBar bar)
             {
+                System.out.println("stop");
             }
         });
-    }
+        int max = seekBar.getMax();
+        float seekBarProgress = max*alpha/255;
+        seekBar.setProgress((int) seekBarProgress);
 
+
+        RatingBar bar = (RatingBar)findViewById(R.id.RatingBar);
+        bar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                image.setImageAlpha((int) rating*255/5);
+            }
+        });
+
+        int max1 = bar.getMax();
+        float RatingBarProgress = max1*alpha/255;
+        bar.setProgress((int)RatingBarProgress);
+    }
 }
---
+++ app/src/main/java/com/example/ancodertest/ViewSwitcherTest/ViewSwitcherTestActivity.java
@@ -0,0 +1,14 @@
+package com.example.ancodertest.ViewSwitcherTest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.ancodertest.R;
+
+public class ViewSwitcherTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_switcher_test);
+    }
+}
